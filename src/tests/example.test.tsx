498dc04727a24780f3552a638bf9124b63465c36
Zaryab Zubair <zaryabz043@gmail.com>
2025-12-26 16:16:13 +0500

    fix: quick fix

--- src/tests/example.test.tsx
+++ src/tests/example.test.tsx
@@ -1,15 +1,15 @@
-import {describe, expect, test} from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-import {render, screen} from './react-tests-utilities'
+import { render, screen } from './react-tests-utilities';
 
 function MyReactComponent() {
-    return <div>My React Component</div>
+  return <div>My React Component</div>;
 }
 
 describe('MyReactComponent', () => {
-    test('given: no props, should: renders a text', () => {
-        render(<MyReactComponent />)
+  test('given: no props, should: renders a text', () => {
+    render(<MyReactComponent />);
 
-        expect(screen.getByText('My React Component')).toBeInTheDocument()
-    })
-})
+    expect(screen.getByText('My React Component')).toBeInTheDocument();
+  });
+});
