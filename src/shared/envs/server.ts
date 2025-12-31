b6235fcba8a4b362f8395a7fdbe75155bfdb77b1
Zaryab Zubair <zaryabz043@gmail.com>
2025-12-31 14:36:54 +0500

    feat: Sentry Setup

--- src/shared/envs/server.ts
+++ src/shared/envs/server.ts
@@ -6,12 +6,12 @@ const serverEnvironmentSchema = z.object({
   PORT: z.string(),
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
-  SENTRY_TOKEN: z.string().optional(),
+  SENTRY_AUTH_TOKEN: z.string().optional(),
 });
 
 export const serverEnvironment = serverEnvironmentSchema.parse({
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  SENTRY_TOKEN: process.env.SENTRY_TOKEN,
+  SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
 });
